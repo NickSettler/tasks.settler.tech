923fd41d88d481316e106a531df4a3ae12eb6778
NickSettler <moiseevnikita14@gmail.com>
2020-09-03 23:05:02 +0500

    Functionality (#3)
    * (dev) added some code
    
    * (ci/cd) removed codeql config
    
    * (ci/cd) changed nodejs config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-document.write('hsh');
+import * as express from "express";
+
+const app = express();
+
+app.get("*", (req, res) => {
+	res.send({
+		result: "Ok"
+	});
+});
+
+app.listen(8087, "0.0.0.0");
