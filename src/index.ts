79f7565966952fab6fb47798a108b7a084fa6f44
NickSettler <moiseevnikita14@gmail.com>
2020-09-04 00:57:39 +0500

    (code) temp changes

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,7 @@ import * as express from "express";
 const app = express();
 
 app.get("*", (req, res) => {
-	res.send({
-		result: "Ok"
-	});
+	res.redirect("http://localhost:8080/");
 });
 
 app.listen(8087, "0.0.0.0");
